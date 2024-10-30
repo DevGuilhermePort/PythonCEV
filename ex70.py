8fc2ff40a93310f1fb27806fa13e7d8f46ecb13c
Guilherme HF Port <guilhermehfport@gmail.com>
2024-10-29 21:47:59 -0300

    Mais anotações

--- ex70.py
+++ ex70.py
@@ -1,12 +1,12 @@
-total = tot_mil = menor = contador = 0
-barato = ''
-while True:
-    produto = str(input('Nome do produto: ')).title().strip()
+total = tot_mil = menor = contador = 0  # Iniciando as 4 variáveis total, tot_mil, menor e contador
+barato = ''  # Iniciando a variável barato
+while True:  # Enquanto for verdadeiro:
+    produto = str(input('Nome do produto: ')).title().strip()  # Nome do produto, removendo todos os espaços na frente e atrás, e convertendo a primeira letra de cada palavra para maiúsculo
     preco = float(input('Preço: '))
-    contador += 1
-    total += preco
-    if preco >= 1000:
-        tot_mil += 1
+    contador += 1  # Contador mais um
+    total += preco  # Total mais preço
+    if preco >= 1000:  # Se preço for maior ou igual a mil:
+        tot_mil += 1  # Tot_mil mais um
     if contador == 1 or preco < menor:  # Quando eu possuo dois blocos iguais eu posso simplificar
         menor = preco
         barato = produto
@@ -14,7 +14,7 @@ while True:
    #     if preco < menor:
    #         menor = preco
    #         barato = produto
-    continuar = ' '
+    continuar = ' '  # Iniciando a variável continuar
     while continuar not in 'SN':  # Se continuar não estiver em 'S' ou 'N':
         continuar = str(input('Quer cotinuar? [S/N] ')).upper().strip()[0]
     if continuar == 'N':  # Se continuar for igual a 'N':
