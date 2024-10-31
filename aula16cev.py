560ef8036ffe07d90674f963c8ed93396f041467
Guilherme HF Port <guilhermehfport@gmail.com>
2024-10-31 11:43:22 -0300

    anotações da aula 16

--- aula16cev.py
+++ aula16cev.py
@@ -7,5 +7,12 @@ for cont in range(0, len(lanche)):  # Criando um loop no renge de zero até o le
     print(lanche[cont], end=' -> ')  # Fatiar um item da tupla no indicie do contador
 print('\n')
 
-for posicao, comida in enumerate(lanche):  # Indicado para usar quado precisa mostrar a posição do elemento.
+for posicao, comida in enumerate(lanche):  # Indicado para usar quando precisa mostrar a posição do elemento.
     print(f'{comida}-{posicao}', end=' -> ')
+print('\n')
+
+a = (4, 5, 4, 8, 9)
+b = (2, 3, 2, 4, 5)
+c = a + b  # Irá concatenar o a no b
+d = b + a  # Irá concatenas o b no a
+print(f'{c}\n{d}')
