66b541cef316148ab5dabbddce016a947c206ff7
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-02 00:23:32 -0300

    Update ex72.py

--- ex72.py
+++ ex72.py
@@ -1,4 +1,8 @@
-numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')  # Criando a tupla com os números escritos por extenso
+numeros = ('zero', 'um', 'dois', 'três', 'quatro',
+           'cinco', 'seis', 'sete', 'oito', 'nove',  # Questões de estética recomendam no máximo 80 linhas
+           'dez', 'onze', 'doze', 'treze', 'quatorze',  # Por comando
+           'quinze', 'dezesseis', 'dezessete', 'dezoito',
+           'dezenove', 'vinte')  # Criando a tupla com os números escritos por extenso
 escolha = int(input('Digite um número entre 0 e 20: '))  # Perguntando o número para o jogador
 while True:  # Enquanto for verdadeiro:
     if escolha >= 0 and escolha <= 20:  # Se a escolha for maior ou igual a zero e menor ou igual a 20:
