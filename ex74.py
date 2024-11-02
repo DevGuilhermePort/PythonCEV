52412d4f5eb3082fdf5577cc7d891ad6bd78f049
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-02 00:15:19 -0300

    Tentativa de realizar o exercicio

--- ex74.py
+++ ex74.py
@@ -1,3 +1,8 @@
-from random import randint
-aleatorios = randint(0, 9)
-print(aleatorios)
+from random import choices
+
+numeros = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+aleatorios = (choices(numeros, k = 5))
+print(f'Os valores sorteados foram: {aleatorios}')
+
+if int(aleatorios[0]) > int(aleatorios[1:]):
+    print(f'O menor valor é {aleatorios[0]}')
