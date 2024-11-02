f141ccda3fca20fdb8a0d48a122f32e8ca7e2516
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-02 00:32:55 -0300

    Melhoria na interatividade

--- ex72.py
+++ ex72.py
@@ -3,9 +3,13 @@ numeros = ('zero', 'um', 'dois', 'três', 'quatro',
            'dez', 'onze', 'doze', 'treze', 'quatorze',  # Por comando
            'quinze', 'dezesseis', 'dezessete', 'dezoito',
            'dezenove', 'vinte')  # Criando a tupla com os números escritos por extenso
-escolha = int(input('Digite um número entre 0 e 20: '))  # Perguntando o número para o jogador
-while True:  # Enquanto for verdadeiro:
-    if 0 <= escolha <= 20:  # Se zero for maior ou igual a escolha e a escolha for menor ou igual a 20
-        break  # Quebre o loop
-    escolha = int(input('Tente novamente. Digite um número entre 0 e 20: '))  # Perguntando de novo
-print(f'Você digitou o número {numeros[escolha]}')  # Escrevendo a respota por extenso
+while True:
+    escolha = int(input('Digite um número entre 0 e 20: '))  # Perguntando o número para o jogador
+    while True:  # Enquanto for verdadeiro:
+        if 0 <= escolha <= 20:  # Se zero for maior ou igual a escolha e a escolha for menor ou igual a 20
+            break  # Quebre o loop
+        escolha = int(input('Tente novamente. Digite um número entre 0 e 20: '))  # Perguntando de novo
+    print(f'Você digitou o número {numeros[escolha]}')  # Escrevendo a respota por extenso
+    continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Opção inválida, tente novamente. Deseja continuar? [S/N] ')).strip().upper()[0]
