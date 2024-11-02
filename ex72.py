ed3c8baa878a516373c75ee54072a23387fac0ce
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-02 00:28:07 -0300

    Melhoria no exercício

--- ex72.py
+++ ex72.py
@@ -5,7 +5,7 @@ numeros = ('zero', 'um', 'dois', 'três', 'quatro',
            'dezenove', 'vinte')  # Criando a tupla com os números escritos por extenso
 escolha = int(input('Digite um número entre 0 e 20: '))  # Perguntando o número para o jogador
 while True:  # Enquanto for verdadeiro:
-    if escolha >= 0 and escolha <= 20:  # Se a escolha for maior ou igual a zero e menor ou igual a 20:
+    if 0 <= escolha <= 20:  # Se zero for maior ou igual a escolha e a escolha for menor ou igual a 20
         break  # Quebre o loop
     escolha = int(input('Tente novamente. Digite um número entre 0 e 20: '))  # Perguntando de novo
 print(f'Você digitou o número {numeros[escolha]}')  # Escrevendo a respota por extenso
