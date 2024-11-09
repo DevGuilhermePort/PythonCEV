55635a39a35d49a44a0bd9145b6b88e175ce2d4b
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-09 01:56:55 -0300

    anotações

--- ex79.py
+++ ex79.py
@@ -1,15 +1,16 @@
-lista = list()
-while True:
-    num = int(input('Digite um número: '))
+lista = list()  # Criando a lista vazia
 
-    if num not in lista:
-         lista.append(num)
-    else:
-         print('Valor duplicado! Não vou adicionar.')
-    continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-    while continuar not in 'SN':
-        continuar = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().upper()[0]
-    if continuar == 'N':
-            break
+while True:  # Enquanto for verdadeiro
+     num = int(input('Digite um número: '))  # Pergunta um número para o usuário
+     if num not in lista:  # Se o número não estiver na lista:
+         lista.append(num)  # Adicionar na lista
+     else:  # Se não:
+         print('Valor duplicado! Não vou adicionar.')  # Falar que já possui o número
+
+     continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]  # Perguntar se quer continuar, tirar todos os espaços na frente e atrás, passar para o maiúsculo e pegar apenas o indicie zero
+     while continuar not in 'SN':
+        continuar = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().upper()[0]  # Perguntar se quer continuar, tirar todos os espaços na frente e atrás, passar para o maiúsculo e pegar apenas o indicie zero
+     if continuar == 'N':  # Se continuar for igual a não:
+            break  # Quebre o loop
 print({'-='  * 20})
-print(f'Você digitou os os valores {lista}')
+print(f'Você digitou os os valores {lista}')  # Lista dos dados
