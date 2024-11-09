4dd5d0d56c84d86c51f29972a77e0a0b81fb4034
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-09 01:52:11 -0300

    Realização nova do ex 78 e 79
    Fazer novamente estes exercícios depois de estudar um pouco facilitou muito a resolução deles e meu entendimento sobre listas

--- ex78.py
+++ ex78.py
@@ -1,13 +1,12 @@
-lista = list()  # Iniciando a minha lista
-for pos in range(0, 5):  # Para cada pos no intervalo de 0 até 5 (efetivando o 5):
-    lista.append(int(input(f'Digite um numero para a Posição {pos + 1}: ')))  # Adicionando um valor a lista 5 vezes
+numeros = []  # Criei a lista vazia
+for cont in range(0, 5):  # Laço de repetição no intervalo de 0 a 5:
+    numeros.append(int(input('Digite um valor: ')))  # Adicionando a entrada no usuário na lista
+print(f'O maior valor digitado foi {max(numeros)} nas posições: ', end='')  # Falando qual foi o maior valor
+for pos, num in enumerate(numeros):  # Enumerando os valores da lista
+    if num == max(numeros):  # Se o número for igual ao maior valor da lista:
+        print(pos, end=' ')  # Sua posição.
 
-print(f'O menor valor digitado foi {min(lista)} nas Posições: ', end='')  # Informando o menor usando a função `min`
-for pos, num in enumerate(lista):  # Para cada pos, num na lista enumerada:
-    if lista[pos] == min(lista):  # Se a lista no indicie pos for igual ao menor:
-        print(f'{pos + 1}', end='...')
-
-print(f'\nO maior valor digitado foi {max(lista)} nas Posições: ', end='')
-for pos, num in enumerate(lista):  # Para cada pos, num na lista enumerada:
-    if lista[pos] == max(lista):  # Se a lista no indicie pos for igual ao maior:
-        print(f'{pos + 1}', end='...')
+print(f'\nO menor valor digitado foi {min(numeros)} nas posições: ', end='')  # Falando qual foi o menor valor
+for pos, num in enumerate(numeros):  # Enumerando os valores da lista
+    if num == min(numeros):  # Se o número for igual ao menor valor da lista: 
+        print(pos, end=' ')  # Sua posição.
--- ex79.py
+++ ex79.py
@@ -1,17 +1,15 @@
-numeros = []  # Criando a lista usando dois colchetes
-while True:  # Enquanto dor verdadeiro:
-    numero = int(input('Digite um valor: '))  # Perguntando um valor e adicionando o valor a uma variável
+lista = list()
+while True:
+    num = int(input('Digite um número: '))
 
-    if numero in numeros:  # Se o número perguntado não estiver na lista:
-        print('Valor duplicado! Não vou adicionar.')  # Não faz nada
-    else:  # Se não:
-        numeros.append(numero)  # Adiciona o valor dado a variável simples na variável composta
-
-    repetir = str(input('Quer continuar? [S/N] ')).strip().upper()[0]  # Perguntando se o programa deve ou não continuar
-    while repetir not in 'SN':  # Se a resposta não for 'S' ou 'N':
-        repetir = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().upper()[0]  # Tente de novo
-    
-    if repetir == 'N':  # Se a respota for 'N':
-        break  # Quebre o loop
-print(f'{'-=' * 30}')
-print(f'Você digitou os valores: {sorted(numeros)}')  # Colocando os números em ordem na tela
+    if num not in lista:
+         lista.append(num)
+    else:
+         print('Valor duplicado! Não vou adicionar.')
+    continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().upper()[0]
+    if continuar == 'N':
+            break
+print({'-='  * 20})
+print(f'Você digitou os os valores {lista}')
