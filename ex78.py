4012324073b0d2b8599ddd164ecc3ec8d18d1a35
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-09 02:27:32 -0300

    Correção basica

--- ex78.py
+++ ex78.py
@@ -1,6 +1,8 @@
 numeros = []  # Criei a lista vazia
 for cont in range(0, 5):  # Laço de repetição no intervalo de 0 a 5:
-    numeros.append(int(input('Digite um valor: ')))  # Adicionando a entrada no usuário na lista
+    numeros.append(int(input(f'Digite um valor para a posição {cont}: ')))  # Adicionando a entrada no usuário na lista
+print(f'Você digitou os valores {numeros}')
+
 print(f'O maior valor digitado foi {max(numeros)} nas posições: ', end='')  # Falando qual foi o maior valor
 for pos, num in enumerate(numeros):  # Enumerando os valores da lista
     if num == max(numeros):  # Se o número for igual ao maior valor da lista:
