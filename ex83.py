e887a98a180e3cc8c988604b15f44aae21fa59c9
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-10 15:19:19 -0300

    Update ex83.py

--- ex83.py
+++ ex83.py
@@ -1,16 +1,16 @@
-expressao = str(input('Digite a expressãp: ')).strip()
-pilha = []
-for simbolo in expressao:
-    if simbolo == '(':
-        pilha.append('(')
-    elif simbolo == ')':
-        if len(pilha) > 0:
-            pilha.pop()
-        else:
-            pilha.append(')')
-            break
+expressao = str(input('Digite a expressão: ')).strip()  # Atribuindo a entrada do usuário à variável expressao
+pilha = []  # Criando a lista pilha
+for simbolo in expressao:  # Para cada elemento(simbolo) na lista(pilha):
+    if simbolo == '(':  # Se o símbolo for '(':
+        pilha.append('(')  # Adicione na lista
+    elif simbolo == ')':  # Se não, se o símbolo for ')':
+        if len(pilha) > 0:  # Se o len de pilha for maior que zero:
+            pilha.pop()  # Remove o último valor
+        else:  # Se não
+            pilha.append(')')  # Adiciona ')' na pilha
+            break  # E quebra o loop
 
-if len(pilha) == 0:
-    print('Sua expressão está válida.')
-else:
-    print('Sua expressão está errada.')
+if len(pilha) == 0:  # Se o len de lista for igual a zero:
+    print('Sua espressão é válida!!!')  # Válido
+else:  # Se não:
+    print('Sua expressão está errada!!!')  # Inválido
