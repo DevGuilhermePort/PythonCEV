d1dcd5c7a33075746dbf40098ae5e4c6666701a7
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-11 17:29:45 -0300

    realização do ex84

--- ex84.py
+++ ex84.py
@@ -1,32 +1,35 @@
 dado = list()
 pessoas = list()
-pesados = list()
 leves = list()
-cont = leve = pesado = 0
-
+pesados = list()
+pesado = 0
+leve = 0
 while True:
-    dado.append(str(input('Nome: ')).strip().title())
+    dado.append(str(input('Nome: ')).strip().capitalize())
     dado.append(float(input('Peso: ')))
     pessoas.append(dado[:])
-    if cont == 0:
-        leve = dado[1]
-        leves.append(leve)
-        pesado = dado[1]
-        pesados.append(pesados)
-    if dado[1] > pesado:
-        pesado = dado[1]
-        pesados.append(dado[0])
-    elif dado[1] < leve:
-        leve = dado[1]
-        leves.append(dado[0])
     dado.clear()
-    cont += 1
 
-    repetir = str(input('Quer continuar: [S/N] ')).strip().upper()[0]
-    while repetir not in 'SN':
-        repetir = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().upper()[0]
-    if repetir == 'N':
+    continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().upper()[0]
+    if continuar == 'N':
         break
-print(f'Ao todo, você cadastrou {len(pessoas)} pessoas.')
-print(f'O maior peso foi {pesado:.2f}Kg. Peso de {pesados}')
-print(f'O menor peso foi {leve:.2f}Kg. Peso de {leves}')
+
+for pos, pessoa in enumerate(pessoas):
+    if pos == 0:
+        leve = pesado = pessoa[1]
+    else:
+        if pessoa[1] > pesado:
+            pesado = pessoa[1]
+        elif pessoa[1] < leve:
+            leve = pessoa[1]
+
+for pessoa in pessoas:
+    if pessoa[1] == pesado:
+        pesados.append(pessoa[0])
+    elif pessoa[1] == leve:
+        leves.append(pessoa[0])
+print(f'Ao todo você cadastrou {len(pessoas)} pessoas.')
+print(f'O maior peso foi de {pesado:.2f}Kg. Peso de {pesados}.')
+print(f'O menor peso doi de {leve:.2f}Kg. Peso de {leves}.')
