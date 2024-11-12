66a05a615948a55349288d67b0aea01a894c0e57
Guilherme HF Port <guilhermehfport@gmail.com>
2024-11-11 21:02:22 -0300

    resolução e anotação ex84 definitivo

--- ex84.py
+++ ex84.py
@@ -1,35 +1,36 @@
-dado = list()
-pessoas = list()
-leves = list()
-pesados = list()
-pesado = 0
-leve = 0
-while True:
-    dado.append(str(input('Nome: ')).strip().capitalize())
-    dado.append(float(input('Peso: ')))
-    pessoas.append(dado[:])
-    dado.clear()
+dados = []  # Criando a lista dados
+principal = []  # E a lista principal
+maior = menor = 0  # Iniciando as variáveis 'maior' e 'menor'
 
-    continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-    while continuar not in 'SN':
-        continuar = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().upper()[0]
-    if continuar == 'N':
-        break
+while True:  # Loop while infinito
+    dados.append(str(input('Nome: ').strip().capitalize()))  # Adicionando na lista uma string do teclado do usuária
+    dados.append(float(input('Peso: ')))  # Adicionando um float na lista
+    
+    if len(principal) == 0:  # Se a lista principal não possui nenhum item dentro:
+        maior = menor = dados[1]  # maior e menor passa a ser 'dados[1]'
+    else:  # Se  não
+        if dados[1] > maior:  # Se dados[1] for maior que 'maior':
+            maior = dados[1]  # Maior passa a ser dados[1]
+        elif dados[1] < menor:  # Se não, se dados[1] for menor que  'menor':
+            menor = dados[1]  # Menor passa a ser dados[1]
+            
+    principal.append(dados[:])  # Adicionando a lista 'dados' na lista 'principal' 
+    dados.clear()  # Limpando a lista 'dados'
 
-for pos, pessoa in enumerate(pessoas):
-    if pos == 0:
-        leve = pesado = pessoa[1]
-    else:
-        if pessoa[1] > pesado:
-            pesado = pessoa[1]
-        elif pessoa[1] < leve:
-            leve = pessoa[1]
+    continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]  # Pedindo uma entrada do usuário, tirando todos os espaços adjacentes, passando para maiúsculo e pegando apenas a primeira letra
+    while continuar not in 'SN':  # Se 'continuar' não for 'S' ou 'N':
+        continuar = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().upper()[0]  # Repetir a pergunta até que a resposta seja válida
+    if continuar == 'N':  # Se a resposta do usuário for 'N':
+        break  # Quebre o loop
 
-for pessoa in pessoas:
-    if pessoa[1] == pesado:
-        pesados.append(pessoa[0])
-    elif pessoa[1] == leve:
-        leves.append(pessoa[0])
-print(f'Ao todo você cadastrou {len(pessoas)} pessoas.')
-print(f'O maior peso foi de {pesado:.2f}Kg. Peso de {pesados}.')
-print(f'O menor peso doi de {leve:.2f}Kg. Peso de {leves}.')
+pesados = []  # Criando a lista 'pesados'
+leves = []  # E 'leves'
+for pos, pessoa in enumerate(principal):  # Cada 'pos' e 'pessoa' na lista 'principal'
+    if pessoa[1] == maior:  # Se pessoa[1] for igual a 'maior':
+        pesados.append(pessoa[0])  # Adiciona pessoa[0] na lista 'pesados'
+    elif pessoa[1] == menor:  # Se pessoa[1] for igual a 'menor':
+        leves.append(pessoa[0])  # Adiciona pessoa[0] na lista 'leves'
+
+print(f'Ao todo você registrou {len(principal)} pessoas.')
+print(f'O maior peso foi de {maior:.2f}Kg. Peso de {pesados}.')
+print(F'O menor peso foi de {menor:.2f}Kg. Peso de {leves}')
