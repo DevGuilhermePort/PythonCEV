efc3cd4231608a185e63d473e856635d1e172cf5
Guilherme HF Port <guilhermehfport@gmail.com>
2024-12-22 20:55:21 -0300

    Recomeço do mundo 3
    Excluindo todos os exercícios do curso de Python 3 - Mundo 3, irei começá-los do zero dando um fim ao meu hiato nos estudos

--- ex74.py
+++
@@ -1,8 +0,0 @@
-from random import randint  # Importando a função randint da biblíoteca random
-
-numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))  # Criando a tupla com 5 números aleatórios de 1 a 10
-print('Os números sorteados foram: ', end='')
-for numero in numeros:  # Para cada numero em número:
-    print(f'{numero} ', end='')
-print(f'\nO maior valor sorteado foi: {max(numeros)}')  # A função max retorna o maior valor de uma sequência ou conjunto de valores
-print(f'O menor valor sorteado foi: {min(numeros)}')  # A função min retorna o menor valor de uma sequência ou conjunto de valores
--- ex77.py
+++
@@ -1,7 +0,0 @@
-palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRÁTIS', 'ESTUDAR',
-            'PRAICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')  # Cada palavra dentro da tupla também é uma variável composta
-for palavra in palavras:  # Para casa palavra em palavras:
-    print(f'\nNa palavra {palavra} temos ', end='')
-    for letra in palavra:  # Para cada letra em palavras:
-        if letra in 'AÁÃÂÀEÉÊÈIÍÎÌOÓÕÒUÚÛÙ':  # Se a letra dor uma vogal:
-            print(f'{letra.lower()}', end='')  # Escreva ela em minúsculo
