aaf5a5f179df25b9eba3bb5c27817f8d15dd4782
Guilherme HF Port <guilhermehfport@gmail.com>
2024-12-22 22:11:22 -0300

    anotações ex73

--- ex73.py
+++ ex73.py
@@ -1,20 +1,18 @@
-times = ('Botafogo', 'Palmeiras', 'Flamengo', 'Fortaleza', 'Internacional', 'São Paulo', 'Corinthians', 'Bahia', 'Cruzeiro', 'Vasco da Gama', 'Vitória', 'Atlético Mineiro', 'Fluminense', 'Grêmio', 'Juventude', 'Bragantino', 'Athletico-PR', 'Criciúma', 'Atletico Goianiense', 'Cuiabá')
+times = ('Botafogo', 'Palmeiras', 'Flamengo', 'Fortaleza', 'Internacional', 'São Paulo', 'Corinthians', 'Bahia', 'Cruzeiro', 'Vasco da Gama', 'Vitória', 'Atlético Mineiro', 'Fluminense', 'Grêmio', 'Juventude', 'Bragantino', 'Athletico-PR', 'Criciúma', 'Atletico Goianiense', 'Cuiabá')  # Criação da tupla com todos os times da serie A do Brasileirão
 
-print(times[0:5])
-
-print(f'{'=-' * 15}')
-
-print(times[-4:])
+print(times[0:5])  # Os cinco primeiros colocados
 
 print(f'{'=-' * 15}')
 
-print(f'{'=-' * 15}')
-
-print(sorted(times))
+print(times[-4:])  # Os quatro últimos colocados
 
 print(f'{'=-' * 15}')
 
-if 'Chapecoense' in times:
-    print(times.index('Chapecoense'))
-else:
-    print('Chapecoense não esta na tabela.')
+print(sorted(times))  # Times em ordem alfabética
+
+print(f'{'=-' * 15}')
+
+if 'Chapecoense' in times:  # Se o time da Chapecoense estiver na lista:
+    print(times.index('Chapecoense'))  # Mostrar em qual posição ele esta
+else:  # Se não:
+    print('Chapecoense não esta na tabela.')  # Falar que ele não esta presente na lista
