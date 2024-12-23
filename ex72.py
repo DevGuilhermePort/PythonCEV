4116bcd9a8afd2c86e9c22b63b6e0c446a626ed1
Guilherme HF Port <guilhermehfport@gmail.com>
2024-12-22 21:56:58 -0300

    anotações ex72

--- ex72.py
+++ ex72.py
@@ -1,8 +1,8 @@
-numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')  # Criação da tupla com os números de 0 á 20 por extenso
 
-resposta = int(input('Digite um número entre 0 e 20: '))
+resposta = int(input('Digite um número entre 0 e 20: '))  # Resposta do usuário
 
-while resposta < 0 and resposta > 20:
-    resposta = int(input('Tente novamnete. Digite um número entre 0 e 20: '))
+while resposta < 0 and resposta > 20:  # Se a resposta for menor que 0 ou maior que 20:
+    resposta = int(input('Tente novamnete. Digite um número entre 0 e 20: '))  # Respota inválida, perguntar de novo
 
-print(f'Você digitou o número {numeros[resposta]}')
+print(f'Você digitou o número {numeros[resposta]}')  # Mostrando a saída da resposta do usuário
