8793b90834e4db2c213a5720aed01d501ebd02b1
Guilherme HF Port <guilhermehfport@gmail.com>
2024-12-29 15:51:44 -0300

    notas do ex77

--- ex77.py
+++ ex77.py
@@ -1,8 +1,8 @@
-palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')  # Criação da tupla com todas as palavras desejadas
 
-for palavra in palavras:
-    print(f'Na palavra {palavra} temos', end=' ')
-    for letra in palavra:
-        if letra in 'AEIOU':
-            print(letra.lower(), end=' ')
-    print()
+for palavra in palavras:  # Para cada 'palavra' em 'palavras':
+    print(f'Na palavra {palavra} temos', end=' ')  # Trocar a quebra de linha por um espaço
+    for letra in palavra:  # Para cada 'letra' na 'palavra':
+        if letra in 'AEIOU':  # Se a letra estiver no conjunto de vogais 'AEIO':
+            print(letra.lower(), end=' ')  # Printar a letra e substituir a quebra de linha por um espaço
+    print()  # Printar uma linha em branco
