3a38131ec74813a3e4daa53f1c3284da07ea76ba
Guilherme HF Port <guilhermehfport@gmail.com>
2024-12-29 18:18:30 -0300

    Update aula17cev.py

--- aula17cev.py
+++ aula17cev.py
@@ -1,25 +1,14 @@
-valores = list()
-for cont in range(0, 5):
-    valores.append(int(input('Digite um valor: ')))  # Posso usar input dentro de append
+valores = []
 
-for valor in valores:
-    print(valor, end=' - ')
-print('\n')
+for c in range(0, 5):
+    valores.append(int(input('Digite um valor: ')))
 
-for pos, v in enumerate(valores):
-    print(f'Encontrei {v} na posição {pos}')
+for pos, valor in enumerate(valores):
+    print(f'Na posução {pos} eu encontrei o valor {valor}')
 
-# PECULIARIDADE DO PYTHON
-lista_a = [1, 2, 3, 4, 5]
-lista_b = lista_a
-lista_b[2] = 2
-print(f'Lista A: {lista_a}')
-print(f'Lista B: {lista_b}')
-# O Python faz uma ligação sempre que você igualar duas listas
-print('')
-# Para fazer uma cópia, devo copiar TODOS os elementos de uma lista para outra
-lista_a = [1, 2, 3, 4, 5]
-lista_b = lista_a[:]
-lista_b[2] = 2
-print(f'Lista A: {lista_a}')
-print(f'Lista B: {lista_b}')
+
+a = [12, 7, 2024]
+b = a[:]
+b[1] = 6
+print(f'Lista B:{b}')
+print(f'Lista A: {a}')
