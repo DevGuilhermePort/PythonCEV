71b3e44c8d601fa1679a0aa58981b1b3c719a0ed
Guilherme HF Port <guilhermehfport@gmail.com>
2024-12-29 15:03:02 -0300

    Update ex75.py

--- ex75.py
+++ ex75.py
@@ -1,4 +1,7 @@
-numeros = (int(input('Digite um número: ')), int(input('Digite outro número: ')) , int(input('Digite mais um número: ')), int(input('Digite o último número: ')))  # Colocando 4 números dados pelo usuário na tupla 'numeros'
+numeros = (int(input('Digite um número: ')),
+           int(input('Digite outro número: ')),
+           int(input('Digite mais um número: ')),
+           int(input('Digite o último número: ')))  # Colocando 4 números dados pelo usuário na tupla 'numeros'
 
 print(f'Você digitou os valores: {numeros}')
 
