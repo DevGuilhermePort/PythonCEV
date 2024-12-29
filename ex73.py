b58d99a2233bde798e0fbeb3c3db23ed095862e3
Guilherme HF Port <guilhermehfport@gmail.com>
2024-12-29 14:45:11 -0300

    Update ex73.py

--- ex73.py
+++ ex73.py
@@ -13,6 +13,6 @@ print(sorted(times))  # Times em ordem alfabética
 print(f'{'=-' * 15}')
 
 if 'Chapecoense' in times:  # Se o time da Chapecoense estiver na lista:
-    print(times.index('Chapecoense'))  # Mostrar em qual posição ele esta
+    print(f'O Chapecoense está na {times.index('Chapecoense') + 1} posição.')  # Mostrar em qual posição ele esta
 else:  # Se não:
     print('Chapecoense não esta na tabela.')  # Falar que ele não esta presente na lista
