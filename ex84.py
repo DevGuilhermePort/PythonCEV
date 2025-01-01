2bcc572f4942acc18c384e80cb2aa88d1e19fc5c
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-01 12:45:36 -0300

    update e anotações ex84

--- ex84.py
+++ ex84.py
@@ -1,49 +1,39 @@
-pessoas = []
-dados = []
-pesados = []
-leves = []
-maior = menor = 0
+pessoas = []  # Criando a lista vazia 'pessoas'
+dados = []  # Criando a lista vazia 'dados'
+pesados = []  # Criando a lista vazia 'pesados'
+leves = []  # Criando a lista vazia 'leves'
+maior = menor = 0  # Iniciando as variáveis 'maior' e 'menor'
 
-while True:
-    dados.append(str(input('Nome: ')).capitalize())
-    dados.append(int(input('Peso: ')))
-    pessoas.append(dados[:])
+while True:  # Loop infinito enquanto for True
+    dados.append(str(input('Nome: ')).capitalize())  # Adicionando uma string em 'dados[0]'
+    dados.append(int(input('Peso: ')))  # Adicionando um inteiro 'dados[1]'
+    pessoas.append(dados[:])  # Fazendo uma cópia dos itens de 'dados' para 'pessoas'
     
-    if len(pessoas) == 1:
-        maior = menor = dados[1]
-    else:
-        if dados[1] > maior:
-            maior = dados[1]
-        elif dados[1] < menor:
-            menor = dados[1]
+    if len(pessoas) == 1:  # Se o len de 'pessoas' for igual à um:
+        maior = menor = dados[1]  # 'maior' e 'menor' passam a ser 'dados[1]'
+    else:  # Se não:
+        if dados[1] > maior:  # Se 'dados[1]' for maior que 'maior':
+            maior = dados[1]  # 'maior' passa a ser 'dados[1]'
+        elif dados[1] < menor:  # Se 'dados[1]' for menor que 'menor':
+            menor = dados[1]  # 'menor' passa a ser 'dados[1]'
 
-    dados.clear()
+    dados.clear()  # Limpando a lista 'dados'
 
-    continuar = str(input('Quer continuar? [S/N] ')).strip().lower()[0]
+    continuar = str(input('Quer continuar? [S/N] ')).strip().lower()[0]  #  Perguntando se o usuário quer continuar, removendo todos os espaçoes antes e depois, jogando para minúsculo e pegando apenas o primeiro indici
 
-    while continuar not in 'sn':
-        continuar = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().lower()[0]
+    while continuar not in 'sn':  # Enquanto 'continuar' não for 's' ou 'n':
+        continuar = str(input('Tente novamente. Quer continuar? [S/N] ')).strip().lower()[0]  # Perguntar novamente
 
-    if continuar == 'n':
-        break
+    if continuar == 'n':  # Se 'continuar' for igual a 'n':
+        break  # Quebre o loop
 
-for pessoa in pessoas:
-    if pessoa[1] == maior:
-        pesados.append(pessoa[0])
-    elif pessoa[1] == menor:
-        leves.append(pessoa[0])
+for pessoa in pessoas:  # Para cada 'pessoa' em 'pessoas':
+    if pessoa[1] == maior:  # Se 'pessoa[1]' for igual à 'maior':
+        pesados.append(pessoa[0])  # Adicionar 'pessoa[0]' na lista 'pesados'
+    elif pessoa[1] == menor:  # Se não, se 'pessoa[1]' for igual à 'menor':
+        leves.append(pessoa[0])  # Adicionar 'pessoa[0]' na lista 'leves'
 
 
-print(f'Ao todo, você cadastrou {"uma pessoa."if len(pessoas) == 1 else f"{len(pessoas)} pessoas."}')
-print(f'O maior peso foi de {maior}Kg. Peso de', end=' ')
-for pessoa in pesados:
-    if pessoa == pesados[len(pesados) - 1]:
-        print(f'{pessoa}.')
-    else:
-        print(pessoa, end=', ')
-print(f'O menor peso foi de {menor}Kg. Peso de', end=' ')
-for pessoa in leves:
-    if pessoa == leves[len(leves) - 1]:
-        print(f'{pessoa}.')
-    else:
-        print(pessoa, end=', ')
+print(f'Ao todo, você cadastrou {"uma pessoa."if len(pessoas) == 1 else f"{len(pessoas)} pessoas."}')  # Operador ternário para simplicar o 'if/else' dentro de uma f-string
+print(f'O maior peso foi de {maior}Kg. Peso de {", ".join(pesados)}.')  # Usando a função join para unir os elementos da lista
+print(f'O menor peso foi de {menor}Kg. Peso de {", ".join(leves)}.')  # Usando a função join para unir os elementos da lista
