dd3a935774a9567635411290168ddd5884ca4847
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-01 16:19:40 -0300

    Update ex88.py

--- ex88.py
+++ ex88.py
@@ -1,14 +1,33 @@
-from random import sample
+from random import randint
 from time import sleep
 
-print(f'{'-' * 30}\n{'JOGUE NA SENA':^30}\n{'-' * 30}')
+jogos = []
+lista = []
 
-jogos = int(input('Quantos jogos quer que eu sorteie? '))
-print(f'{'-=' * 3} SORTEANDO {jogos} JOGOS {'-=' * 3}')
-print('')
-for sorteio in range(1, jogos + 1):
+print(f'{'-' * 30}\n{'JOGUE NA MEGA SENA':^30}\n{'-' * 30}')
+
+jogo = int(input('Quer que eu sorteie quantos jogos? '))
+total = 0
+
+sleep(0.5)
+while total < jogo:
+    contador = 0
+    while True:
+        numero = randint(1, 60)
+        if numero not in lista:
+            lista.append(numero)
+            contador += 1
+        
+        if contador >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
+
+print(f'{'-=' * 3} SORTEANDO {jogo} JOGOS {'-=' * 3}')
+sleep(0.5)
+for pos, valor in enumerate(jogos):
+    print(f'Jogo {pos}: {valor}')
     sleep(0.75)
-    print(f'Jogo {sorteio}: {sample(range(1, 60), 6)}')
-    print('')
-
-print(f'{'-=' * 4} < BOA SORTE > {'-='* 4}')
+print(f'{'-=' * 4} < BOA SORTE > {'-=' * 4}')
