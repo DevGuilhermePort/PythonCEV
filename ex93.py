e350d8f92ab071e6477da0b45f2c3a39395b6216
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-02 18:42:32 -0300

    Comentários no ex93

--- ex93.py
+++ ex93.py
@@ -1,27 +1,24 @@
-total = 0
-gols = []
-jogador = {
+partidas = []  # Criando a lista vazia "partidas"
+jogador = {}  # Criando o dicionário vazio "jogador"
 
-    'nome': str(input('Nome: ')).strip().title()
+jogador['nome'] = str(input("Nome: ")).strip().title()  # Criando a chave "nome" no dicionário
+jogos = int(input(f"Quantas partidas {jogador['nome']} jogou? "))  # Criando a variável "jogos"
+for count in range(0, jogos):  # Para cada "count" no range de zero à "jogos"
+    partidas.append(int(input(f"Quantos gols na partida {count + 1}: ")))  # Adicionar na lista "partidas" o valor da entrada do usuário
+jogador['gols'] = partidas  # Criar a chave "gols" com o valor de "partida" no diconário "jogador"
+jogador['total'] = sum(partidas)  # Criando a chave "total" com o valor da soma dos valores de "partidas" dentro de "jogador"
 
-}
-partidas = int(input(f"Quantas partidas {jogador['nome']} jogou? "))
+print(f"{'-' * 45}\n{jogador}\n{'-' * 45}")
+print("")
 
-for partida in range(0, partidas):
-    gols.append(int(input(f"Quantos gols na partida {partida}? ")))
-    total += gols[partida]
-jogador['gols'] = gols
-jogador['total'] = total
+print("-" * 45)
+for chave, valor in jogador.items():  # Para cada "chave" e "valor" em "jogador.items()":
+    print(f"O campo {chave} tem valor {valor}")
+print("-" * 45)
+print('')
 
-print('-=' * 15)
-print(jogador)
-print('-=' * 15)
-
-for chave, valor in jogador.items():
-    print(f"O campo {chave} tem o valor {valor}.")
-print('-=' * 15)
-
-print(f"O jogador {jogador['nome']} jogou {partidas} partidas.")
-for cont in range(0, partidas):
-    print(f'Na partida {cont}, fez {jogador['gols'][cont]}.')
-print(f'Foi um total de {total} gols.')
+print(f"{'-' * 45}\nO jogador {jogador['nome']} jogou {jogos} partidas.")
+for count in range(0, jogos):  # Para cada "count" no range de zero à "jogos"
+    print(f" => Na partida {count}, fez {jogador['gols'][count]}.")
+print(f"Foi um total de {jogador['total']} gols.")
+print("-" * 45)
