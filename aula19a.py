c736cf710d3780d179563b482f9a466f4bf6ab4b
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-01 23:00:14 -0300

    Update aula19a.py

---
+++ aula19a.py
@@ -0,0 +1,12 @@
+brasil = list()
+estado1 = {'uf': 'Rio de Janeiro', 'Sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(brasil[0])
+print(brasil[1])
+print(brasil)
+print('')
+print(brasil[0]['uf'])
+print(brasil[1]['sigla'])
