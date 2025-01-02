5602212c93fcec4da241c1856cd03a9a95162e7a
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-01 21:51:43 -0300

    Update ex89.py

--- ex89.py
+++ ex89.py
@@ -1,31 +1,31 @@
-aluno = list()
-alunos = list()
+ficha = list()
 
 while True:
-    aluno.append(str(input('Nome: ')).strip().title())
-    aluno.append(float(input('Nota 1: ')))
-    aluno.append(float(input('Nota 2: ')))
-    aluno.append((aluno[1] + aluno[2]) / 2)
-    alunos.append(aluno[:])
-    aluno.clear()
+    nome = str(input('Nome: ')).strip().title()
+    nota_um = float(input('Nota um: '))
+    nota_dois = float(input('Nota dois: '))
+    media = (nota_um + nota_dois) / 2
+    ficha.append([nome, [nota_um, nota_dois], media])
 
-    continuar = str(input('Quer continuar? [S/N] ')).strip().lower()[0]
+    continuar = str(input('Quer continuar?  [S/N] ')).strip().lower()[0]
     while continuar not in 'sn':
-        continuar = str(input('Tente novamente. Quer continuar? [S/N]')).strip().lower()[0]
+        continuar = str(input('Tente novamente. Quer continuar? [S/N] '))
     if continuar == 'n':
         break
 
 print('-' * 30)
-print(f'{'No.':<10}{'Nome':<15}{'Média':>10}')
-for numero, dados in enumerate(alunos):
-    print(f'{numero:<10}{dados[0]:<15}{dados[3]:>10}')
+print(f'{"No.":<6}{"Nome:":<15}{"Média:":>8}')
+for pos, valor in enumerate(ficha):
+    print(f'{pos:<6}{ficha[pos][0]:<15}{ficha[pos][2]:>8}')
 print('-' * 30)
 
-nota = int(input('Mostrar nota de qual aluno? (999 interrompe): '))
-while nota != 999:
+aluno = int(input('Mostrar notas de qual aluno: (999 interrompe): '))
+print('-' * 30)
+while aluno != 999:
+    print(f'Notas {ficha[aluno][0]} de são: {ficha[aluno][1]}')
     print('-' * 30)
-    print(f'Notas de {alunos[nota][0]} são {alunos[nota][1:3]}')
-    nota = int(input('Mostrar nota de qual aluno? (999 interrompe): '))
+    aluno = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    print('-' * 30)
 
 print('FINALIZANDO...')
 print('<<< VOLTE SEMPRE >>>')
