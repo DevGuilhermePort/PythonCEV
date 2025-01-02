bbd68dc738ff214a9781287b2b3ed4ee8406b854
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-01 22:40:16 -0300

    Novas anotações aula19

--- aula19.py
+++ aula19.py
@@ -1,9 +1,36 @@
-dicionario = {'nome': 'Melissa', 'sexo': 'F', 'idade': 19}
-print(f'A {dicionario['nome']} tem {dicionario['idade']} anos.')
+pessoa = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
+print(f'o {pessoa['nome']} tem {pessoa['idade']} anos.')
+print('')
+print(pessoa.keys())
+print('')
+print(pessoa.values())
+print('')
+print(pessoa.items())
 
-dicionario['tamanho'] = 1.60
-print(dicionario.values())
+for k in pessoa.keys():
+    print(k)
+print('')
+for v in pessoa.values():
+    print(v)
+print('')
+for k, v in pessoa.items():
+    print(f'{k} = {k}')
+print('')
 
-del dicionario['tamanho']
-print(dicionario.values())
 
+del pessoa['sexo']
+for k, v in pessoa.items():
+    print(f'{k} = {v}')
+print('')
+
+pessoa['nome'] = 'Leandro'
+
+for k, v in pessoa.items():
+    print(f'{k} = {v}')
+print('')
+
+pessoa['peso'] = 98.5
+
+for k, v in pessoa.items():
+    print(f'{k} = {v}')
+print('')
