960367c94975b62f6c1e4aab7bbc2ffe0d95f603
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-02 23:11:55 -0300

    Update ex95.py

--- ex95.py
+++ ex95.py
@@ -1,21 +1,47 @@
+time = []
 partidas = []
 jogador = {
 
 }
+while True:
+    jogador.clear()
+    partidas.clear()
+    jogador['nome'] = str(input("Nome: ")).strip().title()
+    jogos = int(input(f"Quantos jogos {jogador['nome']} jogou? "))
+    for jogo in range(0, jogos):
+        partidas.append(int(input(f"Quantos gols na partida {jogo + 1}? ")))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(jogador['gols'])
+    time.append(jogador.copy())
 
-jogador['nome'] = str(input("Nome: ")).strip().title()
-jogos = int(input(f"Quantos jogos {jogador['nome']} jogou? "))
-for jogo in range(0, jogos):
-    partidas.append(int(input(f"Quantos gols na partida {jogo + 1}? ")))
-jogador['gols'] = partidas[:]
-jogador['total'] = sum(jogador['gols'])
+    while True:
+        continuar = str(input('Quer continuar? [S/N] ')).strip().lower()[0]
+        if continuar in 'sn':
+            break
+        print("ERRO!!! Por favor, responda apenas com 'S' ou 'N'.")
+    if continuar == 'n':
+        break
 
-print(jogador)
-print("-=" * 15)
-for chave, valor in jogador.items():
-    print(f'O campo {chave} vale {valor}.')
+print("-" * 40)
+print("cod", end='')
+for elemento in jogador.keys():
+    print(f"{elemento:<15}", end='')
+print()
 
-print("-=" * 15)
-for cont in range(0, jogos):
-    print(f"    => Na partida {cont + 1}, fez {jogador['gols'][cont]} gols.")
-print(f"Foi um total de {jogador['total']} gols.")
+print("-" * 40)
+for indicie, valor in enumerate(time):
+    print(f"{indicie:>3}", end='')
+    for dado in valor.values():
+        print(f"{str(dado):<15}", end=' ')
+    print()
+print("-" * 40)
+
+while True:
+    busca = int(input("Mostrar dados de qual jogador? (999 interrompe) "))
+    if busca == 999:
+        break
+    if busca > len(time):
+        print(f"Não existe jogador com o código {busca}")
+    else:
+        for indicie, valor in time[busca]['gols']:
+            print(f"No jogo {indicie + 1} fez {valor} gols.")
