cf880656fd6d8dc0374f2aec072b8a6094666de9
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-04 15:20:25 -0300

    primeiros comentários

--- ex96.py
+++ ex96.py
@@ -1,9 +1,9 @@
-def area(largura, comprimento):
-    area_terreno = largura * comprimento
-    print(f"A área de um terreno {largura:.1f}x{comprimento:.1f} é de {area_terreno:.1f}m²")
+def area(largura, comprimento):  # Definindo a função "area()" pedindo dois parâmetros:
+    area_terreno = largura * comprimento  # "area_terreno" tem o valor do parâmetro "largura" vezes o parâmetro "comprimento"
+    print(f"A área de um terreno {largura:.1f}x{comprimento:.1f} é de {area_terreno:.1f}m²")  # Printando a saída dos dados
 
 
 # Código principal
 print(" Controle de terrenos")
 print("-" * 20)
-area(float(input("Lárgura (m): ")), float(input("Comprimento (m): ")))
+area(float(input("Lárgura (m): ")), float(input("Comprimento (m): ")))  # Chamando a função "area" recebendo duas entradas do usuário como parâmetro
