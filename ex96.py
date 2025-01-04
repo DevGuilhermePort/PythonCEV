6d519a853f2db6a30d6685d596b943f41c4edfa8
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-04 15:17:55 -0300

    Realização ex 96 sozinho

--- ex96.py
+++ ex96.py
@@ -1,3 +1,9 @@
 def area(largura, comprimento):
-    print(" Controle de terrenos")
-    print("-" * 20)
+    area_terreno = largura * comprimento
+    print(f"A área de um terreno {largura:.1f}x{comprimento:.1f} é de {area_terreno:.1f}m²")
+
+
+# Código principal
+print(" Controle de terrenos")
+print("-" * 20)
+area(float(input("Lárgura (m): ")), float(input("Comprimento (m): ")))
