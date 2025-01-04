aa2e941b3b21637bdb4d0f6e10f733a3186296a7
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-04 15:31:55 -0300

    comentários ex97 sozinho

--- ex97.py
+++ ex97.py
@@ -1,10 +1,10 @@
-def escreva(string):
-    print("~" * (len(string) + 2))
+def escreva(string):  # Criando a função "escreva()" pedindo como parâmetro um único valor chamado "string"
+    print("~" * (len(string) + 2))  # Printar "~" de acordo com o tamanho de "string"
     print(f" {string} ")
-    print("~" * (len(string) + 2))
+    print("~" * (len(string) + 2))  # Printar "~" de acordo com o tamanho de "string"
 
 
 # Código principal
-escreva("Guilherme + Melissa")
+escreva("Guilherme + Melissa")  # Chamando o código para teste
 escreva("Projeto: ORION")
 escreva("Primeiro passo")
