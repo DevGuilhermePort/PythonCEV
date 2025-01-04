de9b64ee4feb9bad0f4523c5332460d49d077618
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-04 17:57:28 -0300

    comentários ex 99

--- ex99.py
+++ ex99.py
@@ -1,32 +1,32 @@
-from time import sleep
+from time import sleep  # Importando a função "sleep" da biblíoteca "random"
 
-def maior(*numeros):
-    maior = menor = 0
+def maior(*numeros):  # Definindo a função "maior" fazendo ela receber um empacotamento chamado "numeros"
+    maior = menor = 0  # iniciando tanto "maior" quanto "menor" com zero
     print("-=" * 20)
     print("Analisando os valores passados...")
-    sleep(1)
-    for posicao, numero in enumerate(numeros):
-        if posicao == 0:
-            maior = menor = numero
-            print(numero, end=", ", flush=True)
-            sleep(0.3)
-        elif posicao == len(numeros) - 1:
-            print(f"{numero}.", end=' ', flush=True)
-            sleep(0.3)
-            if numero > maior:
-                maior = numero
-            if numero < menor:
-                menor = numero
-        else:
-            print(numero, end=", ", flush=True)
-            sleep(0.3)
-            if numero > maior:
-                maior = numero
-            if numero < menor:
-                menor = numero
+    sleep(1)  # Dando uma pausa de 1 segundo no código
+    for posicao, numero in enumerate(numeros):  # Para cada "posicao" e "numero" em "numeros" numerado:
+        if posicao == 0:  # Se "posicao" for zero:
+            maior = menor = numero  # "maior" e "menor" recebem "numero"
+            print(numero, end=", ", flush=True)  # Printando numero, trocando o final e atualizando o fluxo para True, para que a saída seja imediata
+            sleep(0.5)  # Dando uma pausa de 0.5 segundos no código
+        elif posicao == len(numeros) - 1:  # Se não, se "posicao" for o len de "numeros" menos um:
+            print(f"{numero}.", end=' ', flush=True)  # Printa "numero" e um ponto final, substitui a quebra de linha e atualiza o fluxo para True, para a saída ser imediata
+            sleep(0.5)  # Pausa o código por 0.5 segundos
+            if numero > maior:  # Se "numero" for maior que "maior":
+                maior = numero  # "maior" passa a ser "numero"
+            if numero < menor:  # Se "numero" for menor que "menor":
+                menor = numero  # "menor" passa a ser "numero"
+        else:  # Se não:
+            print(numero, end=", ", flush=True)  # Printa o "numero", substitui a quebra de linha final e muda o fluxo para True, para o print ter uma saída imediata
+            sleep(0.5)  # Dá uma pausa de 0.5 segundos no código de novo
+            if numero > maior:  # Se "numero" for maior que "maior":
+                maior = numero  # "maior" passa a ser "numero"
+            if numero < menor:  # Se "numero" for menor que "menor":
+                menor = numero  # "menor" passa a ser "numero"
 
-    print(f"Foram informados {len(numeros)} valores ao todo")
-    print(f"O maior valor informado foi {maior}")
+    print(f"Foram informados {len(numeros)} valores ao todo")  # Pritando o len de "numeros"
+    print(f"O maior valor informado foi {maior}")  # Printando o "maior"
 
 
 # Código principal
