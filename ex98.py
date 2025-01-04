c9a750f52bcc9c8172f806da1d1fc869dcd0c09e
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-04 16:35:17 -0300

    Resolução ex98

--- ex98.py
+++ ex98.py
@@ -1,16 +1,33 @@
 from time import sleep
 
 def contador(inicio, fim, passo):
-    if inicio > fim:
-        print(f"Contagem de {inicio} até {fim} pulando de {passo} em {passo}")
-        for num in range(inicio, fim, passo):
-            sleep(0.25)
-            print(num, end=' ')
-        print("Fim!")
-    if fim > inicio:
-        
+    if passo < 0:
+        passo *= 1
+    if passo == 0:
+        passo = 1
 
-print("-=" * 15)
+    print("-=" * 20)
+    print(f"Contagem de {inicio} até {fim} pulando de {passo} em {passo}.")
+    sleep(2)
+    if inicio < fim:
+        contador = inicio
+        while contador <= fim:
+            print(contador, end=" ", flush=True)
+            sleep(0.25)
+            contador += passo
+        print("Fim!")
+    else:
+        contador = inicio
+        while contador >= fim:
+            print(contador, end=" ", flush=True)
+            sleep(0.25)
+            contador -= passo
+        print("Fim!")
+
+
+# Código principal
 contador(1, 10, 1)
-print("-=" * 15)
 contador(10, 0, 2)
+print("-=" * 20)
+print("Agora é sua vez de personalizar a contagem.")
+contador(int(input("Inicio: ")), int(input("Fim: ")), int(input("Passo: ")))
