e7b37d9dde18875a51c6d0c1dc313328f41f2af8
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-04 15:29:32 -0300

    Resolução ex97 sozinho

---
+++ ex97.py
@@ -0,0 +1,10 @@
+def escreva(string):
+    print("~" * (len(string) + 2))
+    print(f" {string} ")
+    print("~" * (len(string) + 2))
+
+
+# Código principal
+escreva("Guilherme + Melissa")
+escreva("Projeto: ORION")
+escreva("Primeiro passo")
