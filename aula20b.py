88abb540c3f0e21e0a1560a15a25bf0eb07c7a77
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-04 01:48:02 -0300

    Comentários aula20b

--- aula20b.py
+++ aula20b.py
@@ -1,10 +1,12 @@
-def dobra(list):
-    pos = 0
-    while pos < len(list):
-        list[pos] *= 2
-        pos += 1
+# Como "desempacotar" nos permite trabalhar apenas com tuplas, nós acabamos tento uma funcionalidade um tanto quanto limitada, justamente pela regra das tuplas. "Tuplas são imutáveis". Como alternativa para isso, nós podemos declarar uma lista inteira como um parâmetro, nos permitindo trabalhar com listas, agora "mutáveis"
+
+def dobra(list):  # Definindo a função "dobra" recebendo um único parametro:
+    pos = 0  # Iniciando a variàvel "pos" com zero
+    while pos < len(list):  # Enquanto "pos" for menor que o tamanho de "list":
+        list[pos] *= 2  # "list" na posição "pos" recebe o dobro
+        pos += 1  # Pos recebe mais um
+    print(list)  # Printando a lista dobrada
 
 
-lista = [6, 3, 9, 1, 0, 2]
-dobra(lista)
-print(lista)
+lista = [12, 7, 20, 24]  # Declarando uma lista de números
+dobra(lista)  # Chamando a função "dobra" e passando como parâmetro a lista "lista"
