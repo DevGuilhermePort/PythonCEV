8baa649ecddb535753a52ec70ed49b814a4a4114
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-04 16:04:21 -0300

    Update ex97.py

--- ex97.py
+++ ex97.py
@@ -1,10 +1,11 @@
 def escreva(string):  # Criando a função "escreva()" pedindo como parâmetro um único valor chamado "string"
-    print("~" * (len(string) + 2))  # Printar "~" de acordo com o tamanho de "string"
-    print(f" {string} ")
-    print("~" * (len(string) + 2))  # Printar "~" de acordo com o tamanho de "string"
+    tamanho = len(string) + 4
+    print("~" * tamanho)  # Printar "~" de acordo com o tamanho de "string"
+    print(f"  {string}  ")
+    print("~" * tamanho)  # Printar "~" de acordo com o tamanho de "string"
 
 
 # Código principal
-escreva("Guilherme + Melissa")  # Chamando o código para teste
+escreva("Guilherme + Melissa == S2")  # Chamando o código para teste
 escreva("Projeto: ORION")
 escreva("Primeiro passo")
