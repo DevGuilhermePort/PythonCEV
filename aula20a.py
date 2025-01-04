ab77635d5f54a434c89d31e072b1a8d24d897b22
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-04 00:59:02 -0300

    Update aula20a.py

--- aula20a.py
+++ aula20a.py
@@ -1,7 +1,7 @@
-def titulo(texto):
-    print("-" * 30)
-    print(f"{texto:^30}")
-    print("-" * 30)
+def titulo(texto):  # Definindo a função "titutlo" com o parâmetro "texto"
+    print("-" * 30)  # Dando suas instruções; Printar "-" trinta vezes
+    print(f"{texto:^30}")  # Printando o parâmetro centralizado em trinta caracteres
+    print("-" * 30)  # Printando mais trinta "-"
 
 
 def contador(*num):
