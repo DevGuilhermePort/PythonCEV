6b8ca8ce18cc6f8820aaf72fb4025f381d1fd0f7
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-06 17:07:20 -0300

    Update ex104.py

--- ex104.py
+++ ex104.py
@@ -1,4 +1,10 @@
-def leiaInt(msg): 
+def leiaInt(msg):   # Criando a função leiaInt() recebendo uma mensagem.
+    """
+    -> Valida a entrada do usuário aceitando apenas números inteiros.
+    : param msg: Recebe a mensagem que vai aparecer quando o número for perguntado.
+    : return: o número, caso ele seja um número realmente, e inteiro.
+    __.__
+    """
     while True:
         numero = str(input(msg))   
         if numero.isnumeric():
