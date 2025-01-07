6e5503ab0d3b3631b49f11b37e3f27248b914625
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-06 21:55:31 -0300

    comentários ex1006

--- ex106.py
+++ ex106.py
@@ -1,24 +1,36 @@
-from time import sleep
+from time import sleep  # Importando a função sleep() da biblíoteca time.
 
-def mensagem(msg, fonte=0, cor=30):
-    tamanho = len(msg)
-    print(f"\033[{fonte};{cor}m{"-" * (tamanho + 4)}\033[m")
-    print(f"\033[{cor}m  {msg}  \033[m")
-    print(f"\033[{fonte};{cor}m{"-" * (tamanho + 4)}\033[m")
+def mensagem(msg, fonte=0, cor=30):  # Criando a função mensagem recebendo um parâmetro obrigatório e dois opcionais.
+    """
+    -> Escreve um "título" de cabeçário centralizado com cor personalizada.
+    : param msg: A mensagem que vai ser escrita.
+    : param fonte: (Opcional) a fonte que vai ser ficar o cabeçário.
+    : param cor: (Opcional) a cor da mensagem toda.
+    __Função feita por Port nos primeiros passos do projeto Orion.__
+    """
+    tamanho = len(msg)  # Tamanho recebendo o len de mensagem.
+    print(f"\033[{fonte};{cor}m{"-" * (tamanho + 4)}\033[m")  # Printando o tamanho do cabeçário com a cor e a fonte personalizada.
+    print(f"\033[{cor}m  {msg}  \033[m")  # Printando a mensagem com a cor escolhida.
+    print(f"\033[{fonte};{cor}m{"-" * (tamanho + 4)}\033[m")  # Printando o tamanho do cabeçário com a cor e a fonte personalizada.
 
 
-def ajuda(comando):
-    mensagem(f"Acessando o manual do comando '{comando}'", cor=32)
-    sleep(2)
-    help(comando)
+def ajuda(comando):  # Definindo a função ajuda() recebendo o parâmetro comando.
+    """
+    -> Mostra a ajuda interativa do python sobre a biblíoteca ou função pedida.
+    : param comando: O comando que você precisa entender.
+    __Função feita por Port nos primeiros passos do projeto Orion.__
+    """
+    mensagem(f"Acessando o manual do comando '{comando}'", cor=32)  # Chamando a função mensagem() dentro da função ajuda().
+    sleep(2)  # dando uma pausa de dois segundos.
+    help(comando)  # Mostrando a ajuda interativa do comando digitado.
 
-while True:
-    mensagem("SISTEMA DE AJUDA PyHELP", 1, 32)
-    resposta = str(input("Função ou biblíoteca: ")).strip().lower()
-    if resposta == "fim":
-            break
-    else:
-        ajuda(resposta)
-    sleep(1)
-    print('')
-mensagem("Adeus!", cor=31)
+while True:  # Loop infinito:
+    mensagem("SISTEMA DE AJUDA PyHELP", 1, 32)  # Chamando a função mensagem().
+    resposta = str(input("Função ou biblíoteca: ")).strip().lower()  # Resposta recebendo a entrada do usuário (a função/biblíoteca que ele deseja ver).
+    if resposta == "fim":  # Se a resposta do usuário for "fim":
+            break  # Quebra o loop.
+    else:  # Se não for "fim":
+        ajuda(resposta)  # Chamando a função ajuda() passando a entrada do usuário como parâmetro.
+    sleep(1)  # Pausando o código por um segundo.
+    print('')  # Linha em branco
+mensagem("Adeus!", cor=31)  # Adeus em vermelho
