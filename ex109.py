f1d29ac69b50e15380915f1130a47ce7f5c1850d
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-07 16:15:13 -0300

    ex 109

--- ex109.py
+++ ex109.py
@@ -4,5 +4,5 @@ num = float(input("Digite um preço: R$"))
 
 print(f"A metade de {moedas.moeda(num)} é {moedas.metade(num, True)}.")
 print(f"O dobro de {moedas.moeda(num)} é {moedas.dobro(num, True)}.")
-print(f"Aumentando 10% temos {moedas.aumentar(num, 10, True)}.")
-print(f"Reduzindo 13% temos {moedas.diminuir(num, 10, True)}.")
+print(f"Aumentando 10% temos {moedas.aumento(num, 10, True)}.")
+print(f"Reduzindo 13% temos {moedas.reducao(num, 10, True)}.")
--- pacote/moedas/__init__.py
+++ pacote/moedas/__init__.py
@@ -1,22 +1,45 @@
 def metade(num=0, format=False):
     resposta = num / 2
-    return resposta
+    if format:
+        return moeda(resposta)
+    else:
+        return resposta
 
 
 def dobro(num=0, format=False):
     resposta = num * 2
-    return resposta
+    if format:
+        return moeda(resposta)
+    else:
+        return resposta
 
 
 def aumento(num=0, taxa=0, format=False):
     resposta = num + (num * taxa / 100)
-    return resposta
+    if format:
+        return moeda(resposta)
+    else:
+        return resposta
 
 
 def reducao(num=0, taxa=0, format=False):
     resposta = num - (num * taxa / 100)
-    return resposta
+    if format:
+        return moeda(resposta)
+    else:
+        return resposta
 
 
 def moeda(num=0, moeda="R$"):
     return f"{moeda}{num:.2f}".replace(".", ",")
+
+
+def resumo(num=0, taxa_aumento=10, taxa_reducao=5):
+    print("-" * 30)
+    print(f"{"RESUMO DO VALOR":^30}")
+    print("-" * 30)
+    print(f"Preço analisado: \t{moeda(num)}.")
+    print(f"Dobro do preço: \t{dobro(num, True)}.")
+    print(f"Metade do preço: \t{metade(num, True)}")
+    print(f"Com {taxa_aumento}% de aumento fica \t{aumento(num, taxa_aumento, True)}")
+    print(f"Com {taxa_reducao}% de redução fica \t{reducao(num, taxa_reducao, True)}")
