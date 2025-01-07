f506543087dd9f18e4ee116e5caecadd010f20ef
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-07 14:29:53 -0300

    nova resolução vdo

--- pacote/moedas/__init__.py
+++ pacote/moedas/__init__.py
@@ -1,42 +1,18 @@
-def metade(num, format=False):
-    if format:
-        return moeda(num / 2)
-    else:
-        return num / 2
+def metade(num):
+    resposta = num / 2
+    return resposta
 
 
-def dobro(num, format=False):
-    if format:
-        return moeda(num * 2)
-    else:
-        return num * 2
+def dobro(num):
+    resposta = num * 2
+    return resposta
 
 
-def aumentar(num, aumento=0, format=False):
-    if format:
-        return moeda(num + (num * (aumento / 100)))
-    else:
-        return num + (num * (aumento / 100))
+def aumento(num, taxa):
+    resposta = num + (num * taxa / 100)
+    return resposta
 
 
-def diminuir(num, reducao=0, format=False):
-    if format:
-        return moeda(num - (num * (reducao / 100)))
-    else:
-        return num - (num * (reducao / 100))
-
-def moeda(num):
-    return f"R${num:.2f}"
-
-
-def resumo(num, aumento=0, reducao=0):
-    print("-" * 30)
-    print(f"{"Resumo do valor":^30}")
-    print("-" * 30)
-    
-    print(f"{"Preço analisado:":<20}{moeda(num)}.")
-    print(f"{"Dobro do preço:":<20}{dobro(num, True)}.")
-    print(f"{"Metade do preço:":<20}{metade(num, True)}.")
-    print(f"{f"{aumento}% de aumento:":<20}{aumentar(num, aumento, True)}.")
-    print(f"{f"{reducao}% de redução:":<20}{diminuir(num, reducao, True)}.")
-    print("-" * 30)
+def reducao(num, taxa):
+    resposta = num - (num * taxa / 100)
+    return resposta
