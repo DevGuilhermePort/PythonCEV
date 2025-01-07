562c20332c002728bf7f0554ea83a148fa176498
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-07 02:46:11 -0300

    ex109 feito

--- pacote/moedas/__init__.py
+++ pacote/moedas/__init__.py
@@ -1,17 +1,29 @@
-def metade(num):
-    return num / 2
+def metade(num, format=False):
+    if format:
+        return moeda(num / 2)
+    else:
+        return num / 2
 
 
-def dobro(num):
-    return num * 2
+def dobro(num, format=False):
+    if format:
+        return moeda(num * 2)
+    else:
+        return num * 2
 
 
-def aumentar(num, aumento):
-    return num + (num * (aumento / 100))
+def aumentar(num, aumento, format=False):
+    if format:
+        return moeda(num + (num * (aumento / 100)))
+    else:
+        return num + (num * (aumento / 100))
 
 
-def diminuir(num, reducao):
-    return num - (num * (reducao / 100))
+def diminuir(num, reducao, format=False):
+    if format:
+        return moeda(num - (num * (reducao / 100)))
+    else:
+        return num - (num * (reducao / 100))
 
 def moeda(num):
     return f"R${num:.2f}"
