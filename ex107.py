20beb69097dd33e1a61df557e932f20127239c3b
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-07 14:37:47 -0300

    anotações 107 new

--- ex107.py
+++ ex107.py
@@ -1,8 +1,8 @@
-from pacote import moedas
+from pacote import moedas  # Importando o módulo moedas do pacote pacote.
 
-numero = float(input("Digite um preço: R$"))
+numero = float(input("Digite o preço: R$"))  # Perguntando um valor ao usuário
 
-print(f"A metade de {numero} é {moedas.metade(numero)}.")
-print(f"O dobro de {numero} é {moedas.dobro(numero)}.")
-print(f"Aumentando 10% temos {moedas.aumentar(numero, 10)}.")
-print(f"Reduzindo 13% temos {moedas.diminuir(numero, 13)}.")
+print(f"A metade de {numero} é {moedas.metade(numero)}.")  # Usando as funções do meu módulo para retornar a metade do número
+print(f"O dobro de {numero} é {moedas.dobro(numero)}.")  # O dobro do número
+print(f"Aumentando 10% temos {moedas.aumento(numero, 10)}.")  # O número com um aumento de 10%
+print(f"Reduzindo 13% temos {moedas.reducao(numero, 13)}.")  # E com uma redução de 13%.
