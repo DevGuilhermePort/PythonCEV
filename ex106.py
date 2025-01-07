b6d7bf62185fd8012b456441ccddb0f047860120
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-06 21:43:13 -0300

    Update ex106.py

--- ex106.py
+++ ex106.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 def mensagem(msg, fonte=0, cor=30):
     tamanho = len(msg)
     print(f"\033[{fonte};{cor}m{"-" * (tamanho + 4)}\033[m")
@@ -5,11 +7,18 @@ def mensagem(msg, fonte=0, cor=30):
     print(f"\033[{fonte};{cor}m{"-" * (tamanho + 4)}\033[m")
 
 
+def ajuda(comando):
+    mensagem(f"Acessando o manual do comando '{comando}'", cor=32)
+    sleep(2)
+    help(comando)
+
 while True:
     mensagem("SISTEMA DE AJUDA PyHELP", 1, 32)
     resposta = str(input("Função ou biblíoteca: ")).strip().lower()
     if resposta == "fim":
             break
     else:
-        print(f"\033[7m{help(resposta)}\033[m",)
+        ajuda(resposta)
+    sleep(1)
     print('')
+mensagem("Adeus!", cor=31)
